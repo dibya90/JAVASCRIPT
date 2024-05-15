c3c153016ee8cf960b103788f2895effd0a195db
dibya90 <142832352+dibya90@users.noreply.github.com>
2024-05-15 07:06:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_Id = 1123333
+let accountEmail = '4325456'
+var accountPassword = '9977766'
+accountCity= "Cuttack"
+let accountState
+
+//account_Id = 2// not allowed
+
+accountEmail = "asdd.com"
+accountPassword = "123433434"
+accountCity = "fsfgrfgf"
+
+console.log(account_Id);
+
+console.table([accountEmail,accountPassword,account_Id,accountCity,accountState])
+
+/* prefer not to use var
+because of issue in block scope nad functional scope
+*/
