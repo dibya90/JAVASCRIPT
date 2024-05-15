46b95920ef452f5805a9cc70921a94c4bcc06a92
dibya90 <142832352+dibya90@users.noreply.github.com>
2024-05-15 08:03:46 +0000

    datatypeConversion

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -12,16 +12,50 @@
 // "33abc" ==>NaN(not a number)
 // true==> 1; false==> 0;
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1==>true; 0==>false
 // string ==>true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+/////***********************OPERATIONS*************//
+let value = 7887
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+// let str1= "I am"
+// let str2= " Dibya"
+
+// let str3= str1+str2
+// console.log(str3)
+
+// console.log("1" +2 );
+// console.log(1+ "2" );
+// console.log("1" + "2" );
+// console.log("1" +2 +2 );
+// console.log(1 +2 +"2" );
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
